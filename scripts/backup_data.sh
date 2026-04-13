582609c1aaa84f6cc0c00af9010a05b1c5ff66a6
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2026-04-12 23:34:44 -0300

    chore: script de backup e upload de cache

---
+++ scripts/backup_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker cp hablemos-espanhol-backend-hablemos-espanhol-backend-1:/app/data/questionsArrayV2.json ~/downloads/
---
+++ scripts/upload_file_docker.sh
@@ -0,0 +1,2 @@
+docker cp ~/temp/questionsArrayV2.json hablemos-espanhol-backend-hablemos-espanhol-backend-1:/app/data/questionsArrayV2.json 
+docker restart hablemos-espanhol-backend-hablemos-espanhol-backend-1
